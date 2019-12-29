6b3cf831ae37563badbefe7139985a7b333ab61e
Code Man <anyone@anywhere.net>
2019-12-30 00:25:00 +0300

    AuthorizationServer Init

---
+++ src/main/java/net/kalush/authorizationjwt/AuthorizationjwtApplication.java
@@ -0,0 +1,13 @@
+package net.kalush.authorizationjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/kalush/authorizationjwt/AuthorizationjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kalush.authorizationjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
